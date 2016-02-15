ede65f4ce08f74e958eb8a5d66a6398944de01c8
Skipper <roychanunkha@gmail.com>
2016-02-15 15:13:24 +0200

    New Migrations
    Here are the added tables

---
+++ database/migrations/2016_02_15_130623_create_article_views_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateArticleViewsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('Article_View', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('Article_View');
+    }
+}
